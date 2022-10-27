ddb7d5dd9a30c807a53dd0246b0628a2a504d92d
Anton Shubin <2spy4x@gmail.com>
2022-10-27 18:22:52 +0800

    ui(calculator): make it actually calculate

--- src/scripts/calculator.ts
+++ src/scripts/calculator.ts
@@ -1,53 +1,80 @@
 import GuiDialog from './_dialog';
+import { onInit } from './_helpers';
 
-// new events
-const dialogClosing = ({ target: dialog }) => {
-  console.log('Dialog closing', dialog);
-};
+interface Values {
+  lpa: number;
+  papr: number;
+  fee: number;
+  psNumber: number;
+  psRange: number;
+  lvtNumber: number;
+  lvtRange: number;
+  lvtType: number;
+}
 
-const dialogClosed = ({ target: dialog }) => {
-  console.log('Dialog closed', dialog);
-  console.info('Dialog user action:', dialog.returnValue);
+const formatCurrency = new Intl.NumberFormat(undefined, {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 0,
+});
 
-  if (dialog.returnValue === 'confirm') {
-    const dialogFormData = new FormData(dialog.querySelector('form'));
-    console.info('Dialog form data', Object.fromEntries(dialogFormData.entries()));
-    // handleFile(dialogFormData);
+onInit(() => {
+  const calculatorEl = document.querySelector('#calculatorDialog');
+  const form = calculatorEl.querySelector('form');
+  const inputEls = {
+    lpa: form.querySelector<HTMLInputElement>('input[name="lpa"]'),
+    papr: form.querySelector<HTMLInputElement>('input[name="papr"]'),
+    fee: form.querySelector<HTMLInputElement>('input[name="fee"]'),
+    psNumber: form.querySelector<HTMLInputElement>('input[type="number"][name="ps"]'),
+    psRange: form.querySelector<HTMLInputElement>('input[type="range"][name="ps"]'),
+    lvtNumber: form.querySelector<HTMLInputElement>('input[type="number"][name="lvt"]'),
+    lvtRange: form.querySelector<HTMLInputElement>('input[type="range"][name="lvt"]'),
+    lvtType: form.querySelector<HTMLSelectElement>('select[name="lvtType"]'),
+  };
+  const resultEl = calculatorEl.querySelector('#calculatorResult');
 
-    dialog.querySelector('form')?.reset();
+  inputEls.psRange.addEventListener('input', () => {
+    if (inputEls.psNumber.value !== inputEls.psRange.value) {
+      inputEls.psNumber.value = inputEls.psRange.value;
+    }
+  });
+  inputEls.psNumber.addEventListener('input', () => {
+    if (inputEls.psRange.value !== inputEls.psNumber.value) {
+      inputEls.psRange.value = inputEls.psNumber.value;
+    }
+  });
+  inputEls.lvtRange.addEventListener('input', () => {
+    if (inputEls.lvtNumber.value !== inputEls.lvtRange.value) {
+      inputEls.lvtNumber.value = inputEls.lvtRange.value;
+    }
+  });
+  inputEls.lvtNumber.addEventListener('input', () => {
+    if (inputEls.lvtRange.value !== inputEls.lvtNumber.value) {
+      inputEls.lvtRange.value = inputEls.lvtNumber.value;
+    }
+  });
+
+  form.addEventListener('input', () => calculate());
+
+  function calculate() {
+    const values = Object.keys(inputEls).reduce((acc, cur) => {
+      acc[cur] = +inputEls[cur].value;
+      return acc;
+    }, {}) as Values;
+    const result =
+      values.lpa *
+      (values.papr / 100 / 365) *
+      (values.fee / 100) *
+      (values.psNumber / 100) *
+      (values.lvtNumber * values.lvtType);
+    resultEl.innerHTML = formatCurrency.format(result);
   }
-};
 
-const dialogOpened = ({ target: dialog }) => {
-  console.log('Dialog open', dialog);
-};
+  calculate();
+});
 
-const dialogOpening = ({ target: dialog }) => {
-  console.log('Dialog opening', dialog);
-};
-
-const dialogRemoved = ({ target: dialog }) => {
-  // cleanup new/optional <dialog> events
-  dialog.removeEventListener('closing', dialogClosing);
-  dialog.removeEventListener('closed', dialogClosed);
-  dialog.removeEventListener('opening', dialogOpening);
-  dialog.removeEventListener('opened', dialogOpened);
-  dialog.removeEventListener('removed', dialogRemoved);
-
-  console.log('Dialog removed', dialog);
-};
-
-// SETUP
 document.querySelectorAll('dialog[modal-mode="mega"]').forEach(dialog => {
-  // sugar up <dialog> elements
   GuiDialog(dialog);
-
-  // new/optional <dialog> events to choose from
-  dialog.addEventListener('closing', dialogClosing);
-  dialog.addEventListener('closed', dialogClosed);
-  dialog.addEventListener('opening', dialogOpening);
-  dialog.addEventListener('opened', dialogOpened);
-  dialog.addEventListener('removed', dialogRemoved);
 });
 
 document.querySelectorAll('dialog[modal-mode="mini"]').forEach(dialog => {
